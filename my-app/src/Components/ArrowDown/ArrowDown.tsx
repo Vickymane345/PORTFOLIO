4b3805d945c44e5418ef1aedc2aa6e0ae6086e61
Vickymane345 <113917597+Vickymane345@users.noreply.github.com>
2025-04-16 23:36:12 +0100

    Update ArrowDown.tsx

--- my-app/src/Components/ArrowDown/ArrowDown.tsx
+++ my-app/src/Components/ArrowDown/ArrowDown.tsx
@@ -1,17 +1,11 @@
+'use client'
 import React, { useContext } from "react";
 import { Fade } from "react-awesome-reveal";
 import { FaArrowDown } from "react-icons/fa";
 import { UserContext } from "../Main/main";
 import Typewriter from "../Typewriter/Typewriter";
 
-function ArrowDown() {import React, { useContext } from "react";
-import { Fade } from "react-awesome-reveal";
-import { FaArrowDown } from "react-icons/fa";
-import { UserContext } from "../Main/main";
-import Typewriter from "../Typewriter/Typewriter";
-
-function ArrowDown()
-{
+function ArrowDown(){
    const context = useContext(UserContext);
   
     if (!context) {
@@ -20,37 +14,6 @@ function ArrowDown()
   const {
     theme
   } = context;
-
-  return (
-    <div>
-      <Fade triggerOnce cascade delay={800} duration={4000}>
-        <div className="text-center pt-2 gap-5 ">
-          <div className="text-center relative h-[4vh] ">
-            <Typewriter text="WANT TO KNOW MORE?" delay={1000} speed={200} />
-          </div>
-
-          <button
-            title="arrow"
-            className={`animate-bounce px-4 cursor-pointer rounded-full border-2 py-4 ${theme ? "bg-[#0a74da] border-[#2a3d52] hover:border-[#3a8fbf] " : "bg-[#007bff] hover:bg-[#0056b3]"}`}
-          >
-            <FaArrowDown className="" />
-          </button>
-        </div>
-      </Fade>
-    </div>
-  );
-}
-
-export default ArrowDown;
-
-  const {
-    theme,
-    handleSectionChange,
-    openNav,
-    handleTheme,
-    handleOpenNav,
-    visibleSection,
-  } = useContext(UserContext);
 
   return (
     <div>
